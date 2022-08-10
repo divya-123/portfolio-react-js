054460324c9f2263ff51529be7e3bd67c2e09931
Divya <divyasrivastava1113@gmail.com>
2022-08-10 20:10:14 +0530

    All sections basic structure completed v1

---
+++ src/components/sections/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+    {
+        
+      title: "Food Order App",
+      subtitle: "e commerce",
+      description:
+      "Enjoy delicious food together with the comfort of home.",  
+      image: './testimonial_1.jpg',
+      link: "https://reactbootcamp.com",
+    },
+    {
+      title: "To-Do List",
+      subtitle: "React and JS",
+      description:
+      "Manage your work with smart To-Do App.",
+     image: "./project-2.gif",
+      link: "https://reedbarger.com",
+    },
+    {
+      title: "Infinite Scroll App",
+      subtitle: "React and Firebase",
+      description:
+      "Infinite scroll feature of social media websites demonstrated.", 
+      image: "./project-3.gif",
+      link: "https://jsbootcamp.com",
+    },
+    {
+      title: "Hooks Project",
+      subtitle: "React Hooks",
+      description:
+      "Practice project for Hand-on in the Hooks feature of React", 
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = ['C', 'JavaScript', 'React', 'Node.js', 'TypeScript'];
+
+  export const testimonials = [
+    {
+        quote: "Very creative and hardworking!!",
+        image: "./testimonial_1.jpg",
+        name: "Anurag Srivastava",
+        company: "TCS"
+    },
+    {
+        quote: "Very good performing person!!",
+        image: "./testimonial_2.jpg",
+        name: "Brij Vaid",
+        company: "IVP"
+    }
+  ];
